4f272482c66bc36aa9de7bf4c9ac30e216036594
i-hidaka <itsuki.hidaka@rakus-partners.co.jp>
2023-08-03 09:12:22 +0900

    CORSエラー解消

--- api/server.ts
+++ api/server.ts
@@ -2,11 +2,13 @@ import * as dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import authRoute from "./router/auth";
+import cors from "cors";
 
 const app = express();
 
 const PORT = 5050;
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoute);
