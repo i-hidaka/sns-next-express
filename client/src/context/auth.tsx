77905399f7bb1bb20756442e97a6600c16870bf8
i-hidaka <itsuki.hidaka@rakus-partners.co.jp>
2023-08-07 11:26:25 +0900

    トークンからユーザーを処理する処理追加

--- client/src/context/auth.tsx
+++ client/src/context/auth.tsx
@@ -1,12 +1,19 @@
 import apiClient from "@/lib/apiClient";
-import React, { ReactNode, useContext, useEffect } from "react";
+import React, { ReactNode, useContext, useEffect, useState } from "react";
 
+interface User {
+  id: number;
+  email: string;
+  username: string;
+}
 interface AuthContextType {
+  user: null | User;
   login: (token: string) => void;
   logout: () => void;
 }
 
 const AuthContext = React.createContext<AuthContextType>({
+  user: null,
   login: () => {},
   logout: () => {},
 });
@@ -16,20 +23,40 @@ export const useAuth = () => {
 };
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const [user, setUser] = useState<null | User>(null);
+
   useEffect(() => {
     const token = localStorage.getItem("auth_token");
-    apiClient.defaults.headers["Authorization"] = `Bearer ${token}`;
+    if (token) {
+      apiClient.defaults.headers["Authorization"] = `Bearer ${token}`;
+
+      apiClient
+        .get("/user/find")
+        .then((res) => {
+          setUser(res.data.user);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
   }, []);
 
   const login = (token: string) => {
     localStorage.setItem("auth_token", token);
+    try {
+      apiClient.get("/users/find").then((res) => {
+        setUser(res.data.user);
+      });
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   const logout = () => {
     localStorage.removeItem("auth_token");
   };
 
-  const value = { login, logout };
+  const value = { login, logout, user };
 
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 };
